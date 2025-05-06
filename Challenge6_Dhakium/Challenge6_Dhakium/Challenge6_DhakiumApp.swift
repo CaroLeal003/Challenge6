401defe30245b2fac44bd194714e94aed30a218e
Carolina Gonzalez <carogonzalezleal@hotmail.com>
2025-05-06 12:25:24 +0200

    Xcode project innit

---
+++ Challenge6_Dhakium/Challenge6_Dhakium/Challenge6_DhakiumApp.swift
@@ -0,0 +1,17 @@
+//
+//  Challenge6_DhakiumApp.swift
+//  Challenge6_Dhakium
+//
+//  Created by Carolina Nicole Gonzalez Leal on 06/05/25.
+//
+
+import SwiftUI
+
+@main
+struct Challenge6_DhakiumApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
---
+++ Challenge6_Dhakium/Challenge6_Dhakium/ContentView.swift
@@ -0,0 +1,24 @@
+//
+//  ContentView.swift
+//  Challenge6_Dhakium
+//
+//  Created by Carolina Nicole Gonzalez Leal on 06/05/25.
+//
+
+import SwiftUI
+
+struct ContentView: View {
+    var body: some View {
+        VStack {
+            Image(systemName: "globe")
+                .imageScale(.large)
+                .foregroundStyle(.tint)
+            Text("Hello, world!")
+        }
+        .padding()
+    }
+}
+
+#Preview {
+    ContentView()
+}
