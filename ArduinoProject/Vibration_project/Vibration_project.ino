a796b7defa0db95afcb92d06b5cc5ca471f7db9e
Carolina Gonzalez Leal <92102316+CaroLeal003@users.noreply.github.com>
2025-05-07 16:16:58 +0200

    Toggle and Slider with Bluetooth

--- ArduinoProject/Vibration_project/Vibration_project.ino
+++ ArduinoProject/Vibration_project/Vibration_project.ino
@@ -1,9 +1,42 @@
-void setup() {
-  // put your setup code here, to run once:
+#include <SoftwareSerial.h>
 
+const int motor_pin = 10;
+
+SoftwareSerial BTSerial(3, 4); // RX, TX
+
+bool motorFromApp = false;
+int command = 0;
+
+void setup() {
+  Serial.begin(9600);
+  BTSerial.begin(9600);
+
+  pinMode(motor_pin, OUTPUT);
+
+  analogWrite(motor_pin, 0);
+}
+
+void checkVibration() {
+  if (BTSerial.available()) {
+    command = BTSerial.parseInt(); 
+
+    if (command == 0) {
+      motorFromApp = false;
+      analogWrite(motor_pin, 0);
+      Serial.println("OFF");
+    } else {
+      motorFromApp = true;
+      Serial.print("ON - Intensity: ");
+      Serial.println(command);
+    }
+  }
 }
 
 void loop() {
-  // put your main code here, to run repeatedly:
+  checkVibration();
 
+  if (motorFromApp) {
+    analogWrite(motor_pin, command);
+  }
 }
+
