f96cc3c47660e571a96358d43fde7b2e8ad52477
Carolina Gonzalez <carogonzalezleal@hotmail.com>
2025-05-07 16:14:44 +0200

    Added the slider

--- Challenge6_Dhakium/Challenge6_Dhakium/ContentView.swift
+++ Challenge6_Dhakium/Challenge6_Dhakium/ContentView.swift
@@ -6,27 +6,45 @@
 //
 
 import SwiftUI
+import Combine
 
 struct ContentView: View {
     @State private var isOn = false
+    @State private var sliderValue: Double = 128
     @StateObject var bluetooth = BluetoothViewModel()
-
+    
+    let timer = Timer.publish(every: 1, on: .main, in: .common).autoconnect()
+    
     var body: some View {
         VStack(spacing: 20) {
             Toggle("Activate", isOn: $isOn)
                 .padding()
                 .onChange(of: isOn) { _, value in
-                    bluetooth.send(command: value ? "1" : "0")
-                    print("Sent: \(value ? "1" : "0")")
+                    let commandToSend = value ? Int(sliderValue) : 0
+                    bluetooth.send(command: "\(commandToSend)\n")
                 }
-            Text("Motor controlled by BLE")
+
+            Slider(value: $sliderValue, in: 0...255, step: 1)
+                .padding(.horizontal)
+                .onChange(of: sliderValue) { _, newValue in
+                    if isOn {
+                        bluetooth.send(command: "\(Int(newValue))\n")
+                    }
+                }
+
+
+            Text("Intensity: \(Int(sliderValue))")
                 .font(.caption)
-                .foregroundColor(.gray)
         }
         .padding()
+        .onReceive(timer) { _ in
+            if isOn {
+                bluetooth.send(command: "\(Int(sliderValue))\n")
+            }
+        }
     }
 }
- 
+
 
 #Preview {
     ContentView()
